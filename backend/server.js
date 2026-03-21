d0a3cee742b4941af2cff2d2aef26b4cea40f052
subiksha <subikshaar18@gmail.com>
2026-03-21 23:12:00 +0530

    last updated server login js

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,10 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production'
-    ? ['https://visionary-muffin-285c95.netlify.app']  // ✅ REMOVE /
-    : [
-        'http://localhost:3000',
-        'http://127.0.0.1:5500',
-        'http://127.0.0.1:5501'
-      ],
-  credentials: true,
+  origin: [
+    'https://visionary-muffin-285c95.netlify.app'
+  ],
+  credentials: true
 }));
 /*app.use(cors({
   origin: [
